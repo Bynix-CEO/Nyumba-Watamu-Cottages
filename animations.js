48ae2cb3fdf45f05ddd063b51c31a8c912d15a54
Bynix-CEO <bynixenterprisesltd@gmail.com>
2024-10-07 08:19:07 +0300

    Added footer and hamburger menu
    Added footer and hamburger menu

--- animations.js
+++ animations.js
@@ -7,3 +7,11 @@ gsap.from(".heroText", {
   scale: 0.2,
   duration: 2,
 });
+
+// JavaScript to toggle the menu
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("open");
+});
